6742af266476b22c56fe737d28592f695e6f7e8d
goodlana <lahee13579@gmail.com>
2021-03-02 23:57:56 +0900

    making game board

---
+++ app.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //card options
+  const cardArray = [
+    {
+      name: "fries",
+      img: "images/fries.png",
+    },
+    {
+      name: "fries",
+      img: "images/fries.png",
+    },
+    {
+      name: "hotdog",
+      img: "images/hotdog.png",
+    },
+    {
+      name: "hotdog",
+      img: "images/hotdog.png",
+    },
+    {
+      name: "cheeseburger",
+      img: "images/cheeseburger.png",
+    },
+    {
+      name: "cheeseburger",
+      img: "images/cheeseburger.png",
+    },
+    {
+      name: "ice-cream",
+      img: "images/ice-cream.png",
+    },
+    {
+      name: "ice-cream",
+      img: "images/ice-cream.png",
+    },
+    {
+      name: "milkshake",
+      img: "images/milkshake.png",
+    },
+    {
+      name: "milkshake",
+      img: "images/milkshake.png",
+    },
+    {
+      name: "pizza",
+      img: "images/pizza.png",
+    },
+    {
+      name: "pizza",
+      img: "images/pizza.png",
+    },
+    {
+      name: "pizza",
+      img: "images/pizza.png",
+    },
+  ];
+
+  const grid = document.querySelector(".grid");
+});
