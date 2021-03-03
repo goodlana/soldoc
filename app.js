7cd892a557ed25cc04f2378f0a9022c56dea8634
goodlana <lahee13579@gmail.com>
2021-03-04 00:50:26 +0900

    complete js part

--- app.js
+++ app.js
@@ -51,21 +51,61 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   ];
 
-  const grid = document.querySelector(".grid");
-  let cardsChosen = [];
-  let cardChosenId = [];
+  cardArray.sort(() => 0.5 - Math.random());
 
+  const grid = document.querySelector(".grid");
+  const resultDisplay = document.querySelector("#result");
+  let cardsChosen = [];
+  let cardsChosenId = [];
+  let cardsWon = [];
+
+  //create your board
   function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
       var card = document.createElement("img");
       card.setAttribute("src", "images/blank.png");
       card.setAttribute("data-id", i);
-      // card.addEventListener("click", flipcard);
+      card.addEventListener("click", flipCard);
       grid.appendChild(card);
     }
   }
 
   //check for matches
+  function checkForMatch() {
+    const cards = document.querySelectorAll("img");
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+
+    //같은 카드를 클릭하면 뒤집는다.
+    if (optionOneId == optionTwoId) {
+      cards[optionOneId].setAttribute("src", "images/blank.png");
+      cards[optionTwoId].setAttribute("src", "images/blank.png");
+      alert("You have clicked the same image!");
+    }
+    // 두 카드의 아이디가 같을 때 === 두 카드가 같을 때
+    //이미지 src를 흰 이미지의 주소로 대체하고, click의 flipCard 이벤트를 remove한다.
+    else if (cardsChosen[0] === cardsChosen[1]) {
+      alert("You found a match");
+      cards[optionOneId].setAttribute("src", "images/white.png");
+      cards[optionTwoId].setAttribute("src", "images/white.png");
+      cards[optionOneId].removeEventListener("click", flipCard);
+      cards[optionTwoId].removeEventListener("click", flipCard);
+      cardsWone.push(cardsChosen);
+    } else {
+      // 두 카드가 다를 때는 뒤집는다.
+      cards[optionOneId].setAttribute("src", "images/blank.png");
+      cards[optionTwoId].setAttribute("src", "images/blank.png");
+      alert("Sorry, try again");
+    }
+
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultDisplay.textContent = cardsWon.length;
+
+    if (cardsWon.length === cardArray.length / 2) {
+      resultDisplay.textContent = "Congratulations! You found them all!";
+    }
+  }
 
   //flip your card
   function flipCard() {
